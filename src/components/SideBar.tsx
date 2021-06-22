f9e8c550290b17c2de4574478bedbc38a7464f76
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-06-21 21:03:09 -0300

    feat: create component sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,31 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+
+interface SideBarProps {
+  data: {
+    genres: GenreResponseProps[];
+    selectedGenreId: number;
+  };
+  handleClickButton(id: number): any;
+}
+
+export function SideBar({ data, handleClickButton }: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {data.genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={data.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
