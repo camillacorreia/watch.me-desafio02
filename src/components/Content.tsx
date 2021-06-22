b5252ea2dd9e7c289195aca7cd1f450f25815728
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-06-21 21:02:56 -0300

    feat: create component content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,33 @@
-export function Content() {
-  // Complete aqui
+import { MovieProps } from '../App';
+import { Header } from "./Header";
+import { MovieCard } from "./MovieCard";
+
+interface Conteudo {
+  content: {
+    title: string;
+    movies: MovieProps[];
+  };
+}
+
+export function Content(props: Conteudo) {
+  return (
+    <>
+      <div className="container">
+        <Header genre={{ title: props.content.title }} />
+        <main>
+          <div className="movies-list">
+            {props.content.movies.map((movie) => (
+              <MovieCard
+                key={movie.imdbID}
+                title={movie.Title}
+                poster={movie.Poster}
+                runtime={movie.Runtime}
+                rating={movie.Ratings[0].Value}
+              />
+            ))}
+          </div>
+        </main>
+      </div>
+    </>
+  );
 }
